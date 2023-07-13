93e1e213def4dc3ca7fe9cf1580275e7b2cea68d
crybaby99 <110852456+crybaby99@users.noreply.github.com>
2023-07-12 23:58:21 -0700

    Create AdminSQLiteHelper.java

---
+++ app/src/main/java/com/example/encuestas/AdminSQLiteHelper.java
@@ -0,0 +1,22 @@
+package com.example.encuestas;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+public class AdminSQLiteHelper extends SQLiteOpenHelper {
+    public AdminSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table articulos(codigo int primary key, descripcion text, precio real)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
+
