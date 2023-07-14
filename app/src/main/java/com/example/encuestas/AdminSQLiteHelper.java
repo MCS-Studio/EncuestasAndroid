7fb5af46db49abc9746f201d805859eff2760abe
Luis Jackniel Arellano <rayreckl.j.a.s01@gmail.com>
2023-07-14 01:02:29 -0700

    all works well

--- app/src/main/java/com/example/encuestas/AdminSQLiteHelper.java
+++ app/src/main/java/com/example/encuestas/AdminSQLiteHelper.java
@@ -13,7 +13,47 @@ public class AdminSQLiteHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("Create table articulos(codigo int primary key, RespuestaA int, RespuestaB int,RespuestaC int,RespuestaD int)");
+        ContentValues valores1 = new ContentValues();
+        ContentValues valores2 = new ContentValues();
+        ContentValues valores3 = new ContentValues();
+        ContentValues valores4 = new ContentValues();
+        ContentValues valores5 = new ContentValues();
 
+
+        valores1.put("codigo", 1);
+        valores1.put("RespuestaA", 0);
+        valores1.put("RespuestaB", 0);
+        valores1.put("RespuestaC", 0);
+        valores1.put("RespuestaD", 0);
+        db.insert("articulos", null, valores1);
+
+        valores2.put("codigo", 2);
+        valores2.put("RespuestaA", 0);
+        valores2.put("RespuestaB", 0);
+        valores2.put("RespuestaC", 0);
+        valores2.put("RespuestaD", 0);
+        db.insert("articulos", null, valores2);
+
+        valores3.put("codigo", 3);
+        valores3.put("RespuestaA", 0);
+        valores3.put("RespuestaB", 0);
+        valores3.put("RespuestaC", 0);
+        valores3.put("RespuestaD", 0);
+        db.insert("articulos", null, valores3);
+
+        valores4.put("codigo", 4);
+        valores4.put("RespuestaA", 0);
+        valores4.put("RespuestaB", 0);
+        valores4.put("RespuestaC", 0);
+        valores4.put("RespuestaD", 0);
+        db.insert("articulos", null, valores4);
+
+        valores5.put("codigo", 5);
+        valores5.put("RespuestaA", 0);
+        valores5.put("RespuestaB", 0);
+        valores5.put("RespuestaC", 0);
+        valores5.put("RespuestaD", 0);
+        db.insert("articulos", null, valores5);
     }
 
 
